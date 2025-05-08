804f9bef33a40b0b6ffe41584901afee15285520
MaryMS25 <mariana.ms.veleiros@gmail.com>
2025-05-08 15:43:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let snakeX = 5, snakeY = 5;
 /* variavel para armazena a velocidade nas direções X e Y, inicalmente em 0, pq a cobra está parada */
 let velocityX = 0, velocityY = 0;
 // uma Array para armazena as coordenadas de cada segmento do corpo, primeiro o elemento é a cabeça
-let snekeBody = [];
+let snakeBody = [];
 // variavel para armazenar o ID do intervalo que será usado para atualizar o jogo em um determinado ritmo.
 let setIntervalId;
 //Uma variavel para manter o controle da pontuação atual do jogador
@@ -52,4 +52,39 @@ const hadleGameOver = () => {
     clearInterval(setIntervalId);
     alert("Game Over! 😫 Aperto Ok para iniciar novamente...");
     location.reload();
+}
+
+// Função para Mudar a direção da cobrinha
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && velocityX != -1)
+        velocityX = 1
+        velocityY = 0;
+}
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key})));
+
+//Começar o Game = init Game
+const initGame = () => {
+    if (gameOver) return hadleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"`;
+
+    //Quando a cobra se alimenta 
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();snakeBody.push([foodY, foodX]);
+        score++;
+        highScore = score >= highScore ? score : highScore;
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerHTML = `Score: ${score}`;
+        highScoreElement.innerHTML = `High Score: ${higtScore}`;
+    }
 }
